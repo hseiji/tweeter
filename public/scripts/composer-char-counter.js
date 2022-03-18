41905f80e91ac450faf7c89e4fd0bcdbc84a6fbd
hseiji <henrique.hst@gmail.com>
2022-03-17 20:31:31 -0400

    create form and counter for characters

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  // The counter updates its value in the output
+  let counter = 140;
+  $("#tweet-text").on('input', function() {
+    const total = counter - this.value.length;
+    if (total < 0) {
+      $("output").css({"color": "red"});
+    } else {
+      $("output").css({"color": "gray"});
+    }
+    $("output").html(total);
+  })
+});
